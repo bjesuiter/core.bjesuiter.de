fdfb62a873a54c82d413f1eb9595cde9a451529e
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-25 11:14:12 +0200

    add auth decoding test

--- routes/ddns/updateCloudflare.ts
+++ routes/ddns/updateCloudflare.ts
@@ -1,5 +1,18 @@
 import { FreshContext } from "$fresh/server.ts";
+import { decodeBase64 } from "@std/encoding";
 
+/**
+ * Request URL Example:
+ * https://core.bjesuiter.de/ddns/updateCloudflare
+ *     ?ip=__MYIP__
+ *     &forHost=__HOSTNAME__
+ *
+ * Auth:
+ * authorization: Basic base64(username:password)
+ *
+ * User Agent:
+ * user-agent: Synology DDNS Updater/72806 support@synology.com
+ */
 export const handler = async (
   _req: Request,
   _ctx: FreshContext,
@@ -11,6 +24,35 @@ export const handler = async (
     headers: Object.fromEntries(_req.headers),
     body: await _req.text(),
   });
+
+  // Step 1 - validate authorization
+  const authHeader = _req.headers.get("authorization");
+  const [authType, authString] = authHeader?.split(" ") ?? [];
+  if (authType !== "Basic") {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  const [username, password] = decodeBase64(authString).split(":");
+  console.debug("Auth", {
+    username,
+    password,
+  });
+  //   if (username !== "ddns" || password !== "ddns") {
+  //     return new Response("Unauthorized", {
+  //       status: 401,
+  //     });
+  //   }
+
+  // Step 2 - validate user agent
+  const userAgent = _req.headers.get("user-agent");
+  if (userAgent !== "Synology DDNS Updater/72806 support@synology.com") {
+    return new Response("Forbidden", {
+      status: 403,
+    });
+  }
+
   return new Response("", {
     status: 200,
   });
