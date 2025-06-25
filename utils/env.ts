cb8d896d76a0a5775a87e3044466bba0166a58b4
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-25 13:27:16 +0200

    init env.ts with fake values when running in github actions build

--- utils/env.ts
+++ utils/env.ts
@@ -8,4 +8,26 @@ const envSchema = z.object({
   CORE_DDNS_PASSWORD: z.string(),
 });
 
-export const env = envSchema.parse(Deno.env.toObject());
+// bjesuiter: make sure, these envs are only loaded when running on deno deploy,
+// NOT when run in a github action build process!
+
+// bjesuiter own inits
+const denoDeploymentId = Deno.env.get("DENO_DEPLOYMENT_ID");
+const isRunningOnDenoDeploy = denoDeploymentId !== undefined &&
+  denoDeploymentId.length > 0;
+if (isRunningOnDenoDeploy) {
+  console.debug(
+    "Detected DENO_DEPLOYMENT_ID - initializing envs",
+  );
+}
+
+// init envs with fake values for github actions
+export const env = isRunningOnDenoDeploy
+  ? envSchema.parse({ ...Deno.env.toObject() })
+  : envSchema.parse({
+    CLOUDFLARE_EMAIL: "github_actions",
+    CLOUDFLARE_DDNS_API_TOKEN: "github_actions",
+    CLOUDFLARE_ZONE_ID_HIBISK_DE: "github_actions",
+    CORE_DDNS_USERNAME: "github_actions",
+    CORE_DDNS_PASSWORD: "github_actions",
+  });
