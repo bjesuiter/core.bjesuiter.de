243a9e63526fd5f0505a9da5a2c431d367f3df69
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-27 17:54:35 +0200

    add more auth functions

--- utils/auth.ts
+++ utils/auth.ts
@@ -16,29 +16,8 @@ export interface SessionWithToken extends Session {
 
 // Token format for client: <SESSION_ID>.<SESSION_SECRET>
 
-function generateSecureRandomString(): string {
-  // Human readable alphabet (a-z, 0-9 without l, o, 0, 1 to avoid confusion)
-  const alphabet = "abcdefghijklmnpqrstuvwxyz23456789";
-
-  // Generate 24 bytes = 192 bits of entropy.
-  // We're only going to use 5 bits per byte so the total entropy will be 192 * 5 / 8 = 120 bits
-  const bytes = new Uint8Array(24);
-  crypto.getRandomValues(bytes);
-
-  let id = "";
-  for (let i = 0; i < bytes.length; i++) {
-    // >> 3 s"removes" the right-most 3 bits of the byte
-    id += alphabet[bytes[i] >> 3];
-  }
-  return id;
-}
-
-async function hashSecret(secret: string): Promise<Uint8Array> {
-  const secretBytes = new TextEncoder().encode(secret);
-  const secretHashBuffer = await crypto.subtle.digest("SHA-256", secretBytes);
-  return new Uint8Array(secretHashBuffer);
-}
-
+// Settings for this module
+// -------------------------
 const sessionExpiresInSeconds = 60 * 60 * 24; // 1 day
 
 /**
@@ -63,8 +42,96 @@ export async function createSession(): Promise<SessionWithToken> {
   await kv.set(["sessions", session.id], {
     id,
     secretHash,
-    createdAt: Math.floor(session.createdAt.getTime() / 1000),
+    createdAt: now,
   });
 
   return session;
+}
+
+async function getSession(sessionId: string): Promise<Session | undefined> {
+  const now = new Date();
+
+  const result = await kv.get(["sessions", sessionId]);
+  if (!result.value) {
+    return undefined;
+  }
+
+  const session = result.value as Session;
+
+  // Check expiration
+  if (
+    now.getTime() - session.createdAt.getTime() >=
+      sessionExpiresInSeconds * 1000
+  ) {
+    await deleteSession(sessionId);
+    return undefined;
+  }
+
+  return session;
+}
+
+export async function validateSessionToken(
+  token: string,
+): Promise<Session | false> {
+  const tokenParts = token.split(".");
+  if (tokenParts.length != 2) {
+    return false;
+  }
+  const sessionId = tokenParts[0];
+  const sessionSecret = tokenParts[1];
+
+  const session = await getSession(sessionId);
+  if (!session) {
+    return false;
+  }
+
+  const tokenSecretHash = await hashSecret(sessionSecret);
+  const validSecret = constantTimeEqual(tokenSecretHash, session.secretHash);
+  if (!validSecret) {
+    return false;
+  }
+
+  return session;
+}
+
+async function deleteSession(sessionId: string): Promise<void> {
+  await kv.delete(["sessions", sessionId]);
+}
+
+/**
+ * Helper functions
+ */
+
+function generateSecureRandomString(): string {
+  // Human readable alphabet (a-z, 0-9 without l, o, 0, 1 to avoid confusion)
+  const alphabet = "abcdefghijklmnpqrstuvwxyz23456789";
+
+  // Generate 24 bytes = 192 bits of entropy.
+  // We're only going to use 5 bits per byte so the total entropy will be 192 * 5 / 8 = 120 bits
+  const bytes = new Uint8Array(24);
+  crypto.getRandomValues(bytes);
+
+  let id = "";
+  for (let i = 0; i < bytes.length; i++) {
+    // >> 3 s"removes" the right-most 3 bits of the byte
+    id += alphabet[bytes[i] >> 3];
+  }
+  return id;
+}
+
+async function hashSecret(secret: string): Promise<Uint8Array> {
+  const secretBytes = new TextEncoder().encode(secret);
+  const secretHashBuffer = await crypto.subtle.digest("SHA-256", secretBytes);
+  return new Uint8Array(secretHashBuffer);
+}
+
+function constantTimeEqual(a: Uint8Array, b: Uint8Array): boolean {
+  if (a.byteLength !== b.byteLength) {
+    return false;
+  }
+  let c = 0;
+  for (let i = 0; i < a.byteLength; i++) {
+    c |= a[i] ^ b[i];
+  }
+  return c === 0;
 }
