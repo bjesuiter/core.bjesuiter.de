bdb0abc062cf4183ca764f4a1f1c2fcfedc10d0a
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-27 23:16:27 +0200

    add users page

--- routes/(authenticated)/users.tsx
+++ routes/(authenticated)/users.tsx
@@ -1,21 +1,32 @@
 import { PageProps } from "$fresh/server.ts";
 import { kv } from "@/utils/kv.ts";
+import { User } from "../../utils/user.type.ts";
 
-interface User {
-  username: string;
-  email: string;
-  passwordHash: string;
-  createdAt: Date;
-  updatedAt: Date;
-  lastLoginAt: Date;
-  lastLoginIp: string;
-}
+// TODO: merge with main users KV
+// interface User {
+//   username: string;
+//   email: string;
+//   passwordHash: string;
+//   createdAt: Date;
+//   updatedAt: Date;
+//   lastLoginAt: Date;
+//   lastLoginIp: string;
+// }
 
 // CAUTION: Cookie based auth needs to be implemented beforehand!
 export default async function UserPage(props: PageProps) {
-  const users = await kv.list<User>({ prefix: ["users"] });
+  const usersAsyncIterator = kv.list<User>({ prefix: ["users"] });
 
-  console.log(users);
+  const users = await Array.fromAsync(usersAsyncIterator);
 
-  return <h1>Platform Users (TODO)</h1>;
+  return (
+    <div>
+      <h1>Platform Users (TODO)</h1>
+      <ul>
+        {users.map((user) => (
+          <li>{user.value.label}&lt;{user.value.email}&gt;</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
