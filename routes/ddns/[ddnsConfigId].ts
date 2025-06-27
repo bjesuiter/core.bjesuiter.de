322b9bbf73e6cfd13ed4070963334f8609b30448
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-06-27 19:36:30 +0200

    add skeleton ddns handling based on given "ddns-config-id" with predefined configs

---
+++ routes/ddns/[ddnsConfigId].ts
@@ -0,0 +1,17 @@
+import { FreshContext } from "$fresh/server.ts";
+
+export const handler = async (
+  req: Request,
+  ctx: FreshContext,
+): Promise<Response> => {
+  const configId = ctx.params.ddnsConfigId;
+  switch (configId) {
+    case "bjesuiter":
+      //   return await updateCloudflare(req, ctx);
+      return new Response("Not implemented yet", { status: 501 });
+    case "ebiko":
+      return new Response("Not implemented yet", { status: 501 });
+    default:
+      return new Response("Not found", { status: 404 });
+  }
+};
--- routes/ddns/[ddnsUser].ts
+++
@@ -1,8 +0,0 @@
-import { FreshContext } from "$fresh/server.ts";
-
-export const handler = async (
-  req: Request,
-  ctx: FreshContext,
-): Promise<Response> => {
-  return new Response("Hello, world!");
-};
