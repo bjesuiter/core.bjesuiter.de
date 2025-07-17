63e2ff7298364e594903e0d0cb4b03d2b7237c85
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-17 04:12:05 +0200

    add config for local deployment (without github actions) + refresh styles

--- routes/(authenticated)/_layout.tsx
+++ routes/(authenticated)/_layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ Component, state }: PageProps) {
   // do something with state here
   return (
     // <div class="bg-[url('/img/cityofgoldcoast-AHOsCAuCnUU.jpg')] bg-cover bg-center h-screen">
-    <div class="bg-linear-to-b from-cyan-100 to-cyan-200 h-screen p-4">
+    <div class="text-teal-800 bg-teal-50 h-screen p-4">
       <Component />
     </div>
   );
