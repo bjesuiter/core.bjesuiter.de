02c2902bd2d075f6db0ca13fe4102e6d7b9ba904
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-17 04:25:49 +0200

    remove hotfix

--- main.ts
+++ main.ts
@@ -9,9 +9,5 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
-import { kv } from "./utils/kv.ts";
-
-// delete superfluous user
-kv.delete(["users", "21b0b61b-13ec-45c0-8be3-8e22c003cec7"]);
 
 await start(manifest, config);
