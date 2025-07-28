8305495ee4e045133d87412055325ca42fa5bb9d
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-28 21:06:45 +0200

    imporve the title of the tab

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,16 @@
 import { type PageProps } from "$fresh/server.ts";
 import { envStore } from "../utils/env_store.ts";
 export default function App({ Component }: PageProps) {
+  let title = "coresvc";
+  if (envStore.STAGE !== "deno_deploy") {
+    title = `${envStore.STAGE} ${title}`;
+  }
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{envStore.STAGE} core.bjesuiter.de</title>
+        <title>{title}</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
