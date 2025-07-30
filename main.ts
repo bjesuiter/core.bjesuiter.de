aa8d9cf07905f289c7375d4918e03ae47cb9a281
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-30 17:45:52 +0200

    add trailing slash handling

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 // Automatically load environment variables from a `.env` file
 import "@std/dotenv/load";
 
-import { App, staticFiles } from "fresh";
+import { App, staticFiles, trailingSlashes } from "fresh";
 
 // bjesuiter: custom code before starting fresh (if needed)
 
 export const app = new App()
   // Add static file serving middleware
   .use(staticFiles())
+  .use(trailingSlashes("never"))
   // Enable file-system based routing
   .fsRoutes();
