361614b63e4da4e4e3a2c0f3312c257ff5713693
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-08-27 10:18:08 +0200

    fix broken import

--- islands/Sidebar.tsx
+++ islands/Sidebar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "preact/hooks";
 import Menu from "../components/menu.tsx";
 import { twJoin } from "tailwind-merge";
-import { Icon } from "../lib/fresh-iconify/Icon.tsx";
 
 /**
  * Contains the sidebar and the floating toolbar to toggle it
