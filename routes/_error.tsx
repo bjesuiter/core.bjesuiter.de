d6b8ce14bc77ab130a01f8f3f24eaf9596c5dc9a
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-08-30 22:53:16 +0200

    convert error page to define.page style

--- routes/_error.tsx
+++ routes/_error.tsx
@@ -1,12 +1,12 @@
-import { HttpError, PageProps } from "fresh";
-import { CoreSvcContext } from "../lib/fresh/defineHelpers.ts";
+import { HttpError } from "fresh";
+import { define } from "../lib/fresh/defineHelpers.ts";
 
-export default function ErrorPage(props: PageProps<undefined, CoreSvcContext>) {
-  const error = props.error; // Contains the thrown Error or HTTPError
+export default define.page((ctx) => {
+  const { error, state } = ctx;
 
   if (error instanceof HttpError) {
     const status = error.status;
-    props.state.tabTitle = `HTTP ${status}`;
+    state.tabTitle = `HTTP ${status}`;
     switch (status) {
       case 404:
         return new Response("404 - Page not found", { status });
@@ -20,21 +20,4 @@ export default function ErrorPage(props: PageProps<undefined, CoreSvcContext>) {
   }
 
   return null;
-
-  // return (
-  //   <>
-  //     <div class="px-4 py-8 h-screen bg-teal-50">
-  //       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-  //         <h1 class="text-4xl font-bold">404 - Page not found</h1>
-  //         <p class="my-4">
-  //           The page you were looking for doesn't exist.
-  //         </p>
-  //         <div class="flex flex-col gap-4">
-  //           <BrowserBackButton>Go one step back</BrowserBackButton>
-  //           <NavButton href="/">Go back home</NavButton>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </>
-  // );
-}
+});
