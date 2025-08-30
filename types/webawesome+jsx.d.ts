bea99785553cbcc0b682f95eb92590dce7326005
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-08-30 15:32:03 +0200

    add webawesome typings

---
+++ types/webawesome+jsx.d.ts
@@ -0,0 +1,3 @@
+// bjesuiter: this is the easiest way to get the JSX types for WebAwesome components
+// Adding them to deno.json > compilerOptions > types[] did not work for me
+import "../node_modules/@awesome.me/webawesome/dist/custom-elements-jsx.d.ts";
