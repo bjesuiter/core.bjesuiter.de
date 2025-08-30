45cfcb58cf8ed2aff4b093ddcb37a652a6276df6
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-08-30 22:57:13 +0200

    convert home.tsx to new define.page style

--- routes/(authenticated)/home.tsx
+++ routes/(authenticated)/home.tsx
@@ -1,16 +1,16 @@
-import { PageProps } from "fresh";
-import { CoreSvcContext } from "@/types/fresh_ctx_state.type.ts";
+import { define } from "../../lib/fresh/defineHelpers.ts";
 
-export default function HomePage(props: PageProps<unknown, CoreSvcContext>) {
+export default define.page((ctx) => {
   const denoDeploymentId = Deno.env.get("DENO_DEPLOYMENT_ID") ?? "unknown";
+  const { user } = ctx.state;
 
   return (
     <div class="border-2 border-zinc-400 rounded-md p-4 shadow-sm bg-zinc-100/50 flex flex-col gap-4 mt-8">
       <h1>Authenticated Homepage</h1>
       <section>
-        <p>Welcome, {props.state.user.label}</p>
-        <p>User Email: {props.state.user.email}</p>
-        <p>User ID: {props.state.user.id}</p>
+        <p>Welcome, {user.label}</p>
+        <p>User Email: {user.email}</p>
+        <p>User ID: {user.id}</p>
       </section>
 
       <hr class="my-4 border-b border-gray-300" />
@@ -22,4 +22,4 @@ export default function HomePage(props: PageProps<unknown, CoreSvcContext>) {
       </footer>
     </div>
   );
-}
+});
