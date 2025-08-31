9a7487f43b6f4e9910deedf417b226d7e3d19bde
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-08-31 22:31:51 +0200

    make sure CoreSvcContext only stores UserFrontend and SessionFrontend types to not spread secrets over my app

--- lib/fresh/defineHelpers.ts
+++ lib/fresh/defineHelpers.ts
@@ -1,10 +1,16 @@
 import { Context, createDefine } from "fresh";
-import { Session } from "@/utils/auth.ts";
-import { User } from "@/utils/user.type.ts";
+import { SessionFrontend } from "../db/schemas/sessions.table.ts";
+import { UserFrontend } from "../db/schemas/users.table.ts";
 
 export interface CoreSvcContext {
-  session: Session;
-  user: User;
+  /**
+   * Note: using SessionFrontend here because the session secret hash is not needed after successful authentication
+   */
+  session: SessionFrontend;
+  /**
+   * Note: using UserFrontend here because the password hash and salt are not needed after successful authentication
+   */
+  user: UserFrontend;
 
   /**
    * Can be used to set the title of the tab in the browser.
