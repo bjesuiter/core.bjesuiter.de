61db229fcd2eaddd00c0eb73f70d0deaafd41da6
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-08-31 21:53:05 +0200

    fix some padding of the main content

--- islands/layout/MainContent.tsx
+++ islands/layout/MainContent.tsx
@@ -11,8 +11,8 @@ export function MainContent(
   return (
     <div
       class={twJoin(
-        "overflow-auto h-full bg-white pt-2.5 pl-4 mt-2 rounded-tl-md border-l border-t border-teal-400 transition-all duration-300 ease-in-out",
-        props.sidebarOpen.value ? "" : "pl-14",
+        "overflow-auto h-full bg-white pt-2.5 pr-4 mt-2 rounded-tl-md border-l border-t border-teal-400 transition-all duration-300 ease-in-out",
+        props.sidebarOpen.value ? "pl-4" : "pl-14",
       )}
     >
       {props.children}
