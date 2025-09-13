76f9900955ca003ab09cee4aee098120bbfa3032
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-09-13 18:30:26 +0200

    move cf and tsrest tests to authenticated section

--- routes/(authenticated)/dev/cf.tsx
+++ routes/(authenticated)/dev/cf.tsx
@@ -1,7 +1,5 @@
-
-
-import { getCfApiClient } from "../../lib/cloudflare/cf_api_client.ts";
-import { define } from "../../lib/fresh/defineHelpers.ts";
+import { getCfApiClient } from "../../../lib/cloudflare/cf_api_client.ts";
+import { define } from "../../../lib/fresh/defineHelpers.ts";
 
 // stub page to test import performance of cf_api_client
 export default define.page(() => {
