d3debbfe01d0192a7dcd03417a47116ed6e58c13
Bloodiko <21989414+Bloodiko@users.noreply.github.com>
2025-09-13 16:52:51 +0200

    add performance routes for api client test

---
+++ routes/dev/tsrest.tsx
@@ -0,0 +1,16 @@
+
+import { initClient } from "@ts-rest/core";
+import { define } from "../../lib/fresh/defineHelpers.ts";
+import { apiCloudflareCom } from "@contracts/api.cloudflare.com/api.cloudflare.com.ts";
+
+// stub page to test import performance of ts-rest client
+export default define.page(() => {
+  const client = initClient(apiCloudflareCom, {
+    baseUrl: "https://api.cloudflare.com/client/v4",
+  })
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+});
