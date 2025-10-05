4b5218fae8ab11aae301ea990688a069d4636877
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-10-05 19:32:51 +0200

    update the schema for ddns_profiles table

--- lib/db/schemas/ddns_profiles.table.ts
+++ lib/db/schemas/ddns_profiles.table.ts
@@ -1,4 +1,5 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { ConnectedServicesTable } from "./connected_services.table.ts";
 import { UsersTable } from "./users.table.ts";
 
 /**
@@ -7,8 +8,16 @@ import { UsersTable } from "./users.table.ts";
 export const DDNSProfilesTable = sqliteTable("ddns_profiles", {
   id: text("id").primaryKey(),
   profile_name: text("profile_name").notNull(),
+  dns_records: text("dns_records", { mode: "json" })
+    .$type<Array<{ record_name: string; zone_id: string }>>()
+    .notNull(),
+  connected_service_id: text("connected_service_id")
+    .references(() => ConnectedServicesTable.id)
+    .notNull(),
+  ddns_username: text("ddns_username").notNull(),
+  ddns_password: text("ddns_password").notNull(),
+  allowed_user_agent: text("allowed_user_agent"),
   createdAt: text("created_at").notNull(),
   updatedAt: text("updated_at").notNull(),
   ownedBy: text("owned_by").references(() => UsersTable.id).notNull(),
-  // TODO: Add dns update rules as text({mode: 'json'}) here
 });
