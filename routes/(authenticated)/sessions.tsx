2265959fca931bc98b5298db17d3fa81b60f5e2c
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-10-05 19:13:40 +0200

    improve data display in current sessions

--- routes/(authenticated)/sessions.tsx
+++ routes/(authenticated)/sessions.tsx
@@ -5,6 +5,7 @@ import { SessionsTable } from "@/lib/db/schemas/sessions.table.ts";
 import { desc, eq } from "drizzle-orm";
 import { UsersTable } from "@/lib/db/schemas/users.table.ts";
 import { define } from "@/lib/fresh/defineHelpers.ts";
+import { format } from "date-fns";
 
 const itemsPerPage = 100;
 
@@ -13,8 +14,19 @@ export default define.page(async (ctx) => {
   //   if (!ctx.state.user.email === envStore.CORE_ROOT_USER_EMAIL) {
   //   }
 
+  const authResult = await ctx.state.authPromise;
+  if (authResult.type === "response") {
+    return authResult.response;
+  }
+  const { session: currentSession } = authResult;
+
   const page = parseInt(ctx.url.searchParams.get("page") ?? "0");
-  const sessions = await db.select().from(SessionsTable).leftJoin(
+  const sessions = await db.select({
+    userId: SessionsTable.userId,
+    userEmail: UsersTable.email,
+    createdAt: SessionsTable.createdAt,
+    sessionId: SessionsTable.id,
+  }).from(SessionsTable).leftJoin(
     UsersTable,
     eq(SessionsTable.userId, UsersTable.id),
   ).orderBy(
@@ -43,17 +55,26 @@ export default define.page(async (ctx) => {
       <table class="text-left">
         <thead>
           <tr>
-            <th>ID</th>
+            <th>Session ID</th>
             <th>User Email</th>
             <th>Created At</th>
+            <th>Current Session</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {sessions.map((session) => (
             <tr>
-              <td>{session.sessions.id}</td>
-              <td>{session.users?.email}</td>
-              <td>{session.sessions.createdAt}</td>
+              <td>{session.sessionId}</td>
+              <td>{session.userEmail}</td>
+              <td class="font-mono text-sm">
+                {format(session.createdAt, "yyyy-MM-dd HH:mm:ss")}
+              </td>
+              <td>
+                {session.sessionId === currentSession.id
+                  ? "Your current session"
+                  : ""}
+              </td>
             </tr>
           ))}
         </tbody>
